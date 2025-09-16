f3b2b3aef12ed1e6aa67746d4ab78753331040aa
thisisaditya17 <joshi.adi1734@gmail.com>
2025-09-16 12:24:15 +0800

    Update the app.py and requirements file

--- app.py
+++ app.py
@@ -114,5 +114,6 @@ def analyze_comments():
     return flask.jsonify(response)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)p
 
